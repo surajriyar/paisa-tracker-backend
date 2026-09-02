7d53346c350dd6de6083ecdcac28a0419de82c59
Suraj Riar <macintosh@192.168.1.15>
2026-09-03 00:12:03 +0530

    Use Firebase secret file

--- server.js
+++ server.js
@@ -18,26 +18,13 @@ const {
   getAuth,
 } = require("firebase-admin/auth");
 
-const serviceAccount = {
-  type: process.env.FIREBASE_TYPE,
-  project_id: process.env.FIREBASE_PROJECT_ID,
-  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
-  client_email: process.env.FIREBASE_CLIENT_EMAIL,
-  client_id: process.env.FIREBASE_CLIENT_ID,
-  auth_uri: process.env.FIREBASE_AUTH_URI,
-  token_uri: process.env.FIREBASE_TOKEN_URI,
-  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
-  client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
-  universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN,
-};
+const serviceAccount = require("/etc/secrets/firebase-service-account.json");
 
 const firebaseAdmin = initializeApp({
   credential: cert(serviceAccount),
 });
 
 const firebaseAuth = getAuth(firebaseAdmin);
-
 
 
 // =====================================================
